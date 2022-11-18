f334e84d1d3ce7d8bdab39629436144f031d4310
isyed01 <isyed01@users.noreply.github.com>
2022-11-18 03:51:06 +0000

    deploy: 97a379cf193398767346774aeeda26663005d318

--- src/components/timers/Tabata.js
+++
@@ -1,9 +0,0 @@
-import { CONFIG_TABATA } from '../../constants.js';
-import TimeCounter from './TimeCounter';
-
-const Tabata = () => {
-    return <div className="wrapper" style={{ width: 400 }}>
-        <TimeCounter conf={CONFIG_TABATA} />
-        </div>
-};
-export default Tabata;
--- src/components/timers/XY.js
+++
@@ -1,9 +0,0 @@
-import { CONFIG_XY } from '../../constants.js';
-import TimeCounter from './TimeCounter';
-
-const XY = () => {
-    return <div className="wrapper" style={{ width: 400 }}>
-        <TimeCounter conf={CONFIG_XY} />
-        </div>
-};
-export default XY;
